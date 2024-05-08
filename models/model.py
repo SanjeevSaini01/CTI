e15d2204d0ee2b69491ee958b1eea62d40d6b655
SanjeevSaini01 <168521480+SanjeevSaini01@users.noreply.github.com>
2024-05-08 23:15:28 +0530

    Add files via upload

---
+++ models/model.py
@@ -0,0 +1,100 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+
+
+def model_a(feature_layer):
+    model = tf.keras.Sequential()
+    model.add(feature_layer)
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dense(16, kernel_initializer = 'uniform' ,kernel_regularizer = tf.keras.regularizers.l2(0.02), activation = 'relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dropout(0.2))
+    model.add(layers.Dense(32, kernel_initializer = 'uniform' ,kernel_regularizer = tf.keras.regularizers.l2(0.02), activation = 'relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dropout(0.2))
+    model.add(layers.Dense(64, kernel_initializer = 'uniform' ,kernel_regularizer = tf.keras.regularizers.l2(0.02), activation = 'relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dropout(0.2))
+    model.add(layers.Dense(1, activation='sigmoid'))
+
+    return model
+
+
+def model_b(feature_layer):
+    model = tf.keras.Sequential()
+    model.add(feature_layer)
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dense(16, kernel_initializer = 'uniform' ,kernel_regularizer = tf.keras.regularizers.l2(0.01), activation = 'relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dropout(0.3))
+    model.add(layers.Dense(32,kernel_initializer = 'uniform', kernel_regularizer = tf.keras.regularizers.l2(0.01), activation = 'relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dropout(0.3))
+    model.add(layers.Dense(64,kernel_initializer = 'uniform', kernel_regularizer = tf.keras.regularizers.l2(0.01), activation = 'relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dropout(0.3))
+    model.add(layers.Dense(128,kernel_initializer = 'uniform', kernel_regularizer = tf.keras.regularizers.l2(0.01), activation = 'relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dropout(0.3))
+    model.add(layers.Dense(1, activation='sigmoid'))
+
+    return model
+
+
+
+def model_c(feature_layer):
+    model = tf.keras.Sequential()
+    model.add(feature_layer)
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dense(16,kernel_regularizer = tf.keras.regularizers.l2(0.02), activation = 'relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dropout(0.2))
+    model.add(layers.Dense(32,kernel_regularizer = tf.keras.regularizers.l2(0.02), activation = 'relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dropout(0.2))
+    model.add(layers.Dense(64,kernel_regularizer = tf.keras.regularizers.l2(0.02), activation = 'relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dropout(0.2))
+    model.add(layers.Dense(1, activation='sigmoid'))
+
+    return model
+
+
+def model_d(feature_layer):
+    model = tf.keras.Sequential()
+    model.add(feature_layer)
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dense(16,kernel_regularizer = tf.keras.regularizers.l2(0.02), activation = 'relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dropout(0.3))
+    model.add(layers.Dense(32,kernel_regularizer = tf.keras.regularizers.l2(0.02), activation = 'relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dropout(0.3))
+    model.add(layers.Dense(64,kernel_regularizer = tf.keras.regularizers.l2(0.02), activation = 'relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dropout(0.3))
+    model.add(layers.Dense(128,kernel_regularizer = tf.keras.regularizers.l2(0.02), activation = 'relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dropout(0.3))
+    model.add(layers.Dense(1, activation='sigmoid'))
+
+    return model
+
+
+
+def model_e(feature_layer):
+    model = tf.keras.Sequential()
+    model.add(feature_layer)
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dense(16,kernel_regularizer = tf.keras.regularizers.l2(0.02), activation = 'relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dropout(0.2))
+    model.add(layers.Dense(64,kernel_regularizer = tf.keras.regularizers.l2(0.02), activation = 'relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dropout(0.2))
+    model.add(layers.Dense(128,kernel_regularizer = tf.keras.regularizers.l2(0.02), activation = 'relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dropout(0.2))
+    model.add(layers.Dense(1, activation='sigmoid'))
+
+    return model
---
+++ utils/util.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import tensorflow as tf
+from tensorflow import feature_column
+
+def file_reader():
+    data = pd.read_csv('D:\\Sanjeev_Disertation\\UNSW_NB15_training-set.csv')
+    data_ = pd.read_csv('D:\\Sanjeev_Disertation\\UNSW_NB15_testing-set.csv')
+    data = pd.concat([data, data_], ignore_index=True)
+    return data
+
+def data_preparation(data):
+    data_features = data
+    if set(['id','attack_cat','label']).issubset(set(data_features.columns)):
+        data_features = data_features.drop(['id','attack_cat','label'], axis=1)
+    if set(['label']).issubset(set(data.columns)):
+        data_labels = data.pop('label')
+    
+    return data_features,data_labels
+
+def  shuffle_dataset(data_features,data_labels,batch_size,shuffle=True):
+    ds = tf.data.Dataset.from_tensor_slices((dict(data_features),data_labels))
+    if shuffle:
+        ds = ds.shuffle(buffer_size=len(data_features))
+    ds = ds.batch(batch_size)
+    return ds
+
+def column_transformation(training_data_features,dataframe):
+    feature_columns = []
+    categorical_columns = []
+    numeric_columns =  []
+    float_columns = []
+    dtypes = training_data_features.dtypes.to_dict()
+    for column,type in dtypes.items():
+        if type == 'float64':
+            float_columns.append(column)
+        elif type == 'int64':
+            numeric_columns.append(column)
+        elif type == 'O':
+            categorical_columns.append(column)
+    for column_name in numeric_columns:
+        feature_columns.append(feature_column.numeric_column(column_name))
+    for column_name in float_columns:
+        feature_columns.append(feature_column.numeric_column(column_name))
+    for column_name in categorical_columns:
+        categorical_column = feature_column.categorical_column_with_vocabulary_list(column_name, dataframe[column_name].unique())
+        indicator_column = feature_column.indicator_column(categorical_column)
+        feature_columns.append(indicator_column)
+    return feature_columns
